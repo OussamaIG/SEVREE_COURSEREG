37cc91705c89d6b6e63d36c61b12755eaa908f50
OussamaIG <oussig7@yahoo.com>
2025-08-10 17:32:22 +0100

    fixed js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,24 @@
 const btn = document.querySelector(".readall")
 const hiddenclasses = document.getElementsByClassName("hidden")
 
-console.log(hiddenclasses)
-
+let myclose = false
 
 btn.addEventListener("click", ()=>{
-    Array.from(hiddenclasses).forEach(element => {
+    if(myclose == false){
+        Array.from(hiddenclasses).forEach(element => {
         element.classList.remove("hidden")
-        element.classList.add("row")
+        element.classList.add("row2")
+        btn.textContent = "Close"
+        myclose = true
+    });}
+    else{
+        console.log("hi")
+        const deletedelms = document.getElementsByClassName("row2")
+        Array.from(deletedelms).forEach(element => {
+        element.classList.remove("row2")
+        element.classList.add("hidden")
+        btn.textContent = "Close"
+        myclose = false
     });
+    }      
 })
